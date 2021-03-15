e73f3f3b5eb4367e941d636d6b3b46b855717c0a
Victorowsky <Victorowskyy@gmail.com>
2021-03-15 20:40:09 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import FirstContent from './comp/FirstContent./FirstContent';
+import Header from './comp/Header/Header';
+import "./App.css"
+import React, { useState, useEffect, useCallback } from 'react';
+import SecondContent from './comp/SecondContent/SecondContent';
+import ThirdContent from './comp/ThirdContent/ThirdContent';
+import Footer from './comp/Footer/Footer';
+
+export const StateContext = React.createContext()
+
 
 function App() {
+  
+  const [isMobileVersion, setIsMobileVersion] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+
+  const resizeFunction = useCallback(() =>{
+    if(window.innerWidth < 801){
+        if(!isMobileVersion){
+            setIsMobileVersion(true)
+        }
+    }else{
+        if(isMobileVersion){
+            setIsMobileVersion(false)
+            setIsMobileMenuOpen(false)
+        }
+
+    }
+},[isMobileVersion])
+
+useEffect(()=>{
+    resizeFunction()
+    window.addEventListener('resize', resizeFunction)
+    return ()=>{
+        window.removeEventListener('resize', resizeFunction)
+    }
+},[resizeFunction])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StateContext.Provider value={{isMobileVersion, setIsMobileVersion,isMobileMenuOpen, setIsMobileMenuOpen}}>
+        <div className="app">
+          <Header/>
+          <FirstContent/>
+          <SecondContent/>
+          <ThirdContent/>
+          <Footer/>
+        </div>
+    </StateContext.Provider>
+  
   );
 }
 
---
+++ src/comp/Footer/FooterColumn.js
@@ -0,0 +1,19 @@
+import "./FooterColumn.css"
+const FooterColumn = ({type, list}) => {
+
+    console.log(list);
+    const createList = list.map(item=>{
+        return (<li key={item}>{item}</li>)
+    })
+
+    return ( 
+        <div className="footerColumn">
+                <p className="footerBolder">{type}</p>
+                <ul>
+                    {createList}
+                </ul>
+        </div>
+     );
+}
+ 
+export default FooterColumn;
